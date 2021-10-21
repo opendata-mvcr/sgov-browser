46c9fe43f4d347e720b7bb0fdab8407c6f5e394e
FilipKopecky <kopeckyfil@gmail.com>
2021-10-21 10:41:05 +0200

    [#45] Error message displayed
    Error message shown when terms are not able to be fetched

--- src/components/VocabularyTerms.tsx
+++ src/components/VocabularyTerms.tsx
@@ -3,20 +3,27 @@ import { useVocabularyTerms } from "../api/VocabularyAPI";
 import Loader from "./Loader";
 import VocabularyTermsList from "./VocabularyTermsList";
 import { DetailItemWrapper } from "./Hierarchy";
+import { Typography } from "@material-ui/core";
 
 interface UriItem {
   uri: string;
 }
 
 const VocabularyTerms: React.FC<UriItem> = (props) => {
-  const { data = [], isLoading } = useVocabularyTerms(props.uri);
-  if (isLoading) {
+  const { data = [], isLoading, isError } = useVocabularyTerms(props.uri);
+
+  if (isError || isLoading) {
     return (
       <DetailItemWrapper title={"Pojmy"}>
-        <Loader size={50} variant="h5" />
+        {isLoading ? (
+          <Loader size={50} variant="h5" />
+        ) : (
+          <Typography variant="h6">Při načítání pojmů došlo k chybě</Typography>
+        )}
       </DetailItemWrapper>
     );
   }
+
   return <VocabularyTermsList terms={data} />;
 };
 
