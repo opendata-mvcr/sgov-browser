42cd11dbff6aa6d13842da2d5d73b84d23754334
FilipKopecky <kopeckyfil@gmail.com>
2021-11-04 23:37:25 +0100

    [#69 #70] Search persistence + search feedback

--- src/components/LargeSearchBar.tsx
+++ src/components/LargeSearchBar.tsx
@@ -6,16 +6,11 @@ interface LargeSearchBarProps {
   searchedText: string | null;
 }
 
-const LargeSearchBar: React.FC<LargeSearchBarProps> = (props) => {
+const LargeSearchBar: React.FC<LargeSearchBarProps> = ({ searchedText }) => {
   return (
     <Box bgcolor="primary.main" py={2}>
       <Container>
-        <SearchBar size="large" />
-        <Box pl={6} pt={2}>
-          <Typography variant="h5" color="textSecondary">
-            Bohužel jsme nenalezli přesný význam slova "{props.searchedText}"
-          </Typography>
-        </Box>
+        <SearchBar size="large" initialValue={searchedText ?? ""} />
       </Container>
     </Box>
   );
