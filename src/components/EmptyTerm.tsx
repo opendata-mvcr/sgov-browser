bf52b01b09fccd4ea6be9025bc0107a1f440bf44
FilipKopecky <kopeckyfil@gmail.com>
2021-11-19 16:55:50 +0100

    [#83] Implemented empty-term component

--- src/components/EmptyTerm.tsx
+++ src/components/EmptyTerm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography } from "@mui/material";
 import { ReactComponent as NoData } from "../assets/no_data.svg";
 
 const EmptyTerm: React.FC = () => {
