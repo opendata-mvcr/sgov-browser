2bebb72f7e8b0c04726852f67b3af069ee71bad5
FilipKopecky <kopeckyfil@gmail.com>
2021-11-19 13:04:21 +0100

    [#23] Removed lab and fix one last component

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,26 +1,24 @@
 import React from "react";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Grid, styled, Typography } from "@mui/material";
 import opzLogo from "./assets/opz_logo.svg";
 import { ReactComponent as FooterImage } from "./assets/footer_image.svg";
-import makeStyles from "@mui/styles/makeStyles";
 
-const useStyles = makeStyles((theme) => ({
-  wrapper: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    [theme.breakpoints.down("sm")]: {
-      display: "block",
-    },
+const Illustration = styled(FooterImage)(({ theme }) => ({
+  position: "absolute",
+  bottom: "-16px",
+  right: "60px",
+  maxHeight: 115,
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
   },
-  illustration: {
-    position: "absolute",
-    bottom: "-16px",
-    right: "60px",
-    maxHeight: 115,
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
-    },
+}));
+
+const Wrapper = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  [theme.breakpoints.down("sm")]: {
+    display: "block",
   },
 }));
 
@@ -29,27 +27,25 @@ interface FooterProps {
 }
 
 const Footer: React.FC<FooterProps> = (props) => {
-  const classes = useStyles();
-
   return (
     <Box>
       {props.showImage && (
         <Container>
           <Box style={{ position: "relative", marginTop: 100 }}>
-            <FooterImage className={classes.illustration} />
+            <Illustration />
           </Box>
         </Container>
       )}
       <Box bgcolor="primary.main" py={1}>
         <Container maxWidth="lg">
-          <Box className={classes.wrapper}>
+          <Wrapper>
             <Typography variant="body2" color="textSecondary">
               Tato aplikace je udržována v rámci projektu
               <br />
               OPZ č. CZ.03.4.74/0.0/0.0/15_025/0013983.
             </Typography>
             <img src={opzLogo} alt="OPZ" height={50} />
-          </Box>
+          </Wrapper>
         </Container>
       </Box>
     </Box>
